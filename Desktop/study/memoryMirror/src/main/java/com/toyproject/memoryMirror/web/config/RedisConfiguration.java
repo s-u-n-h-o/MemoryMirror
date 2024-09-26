ae9050770ce1177b74124a5acf89a2c3ab0c6e5b
s-u-n-h-o <chltjsgh06@gmail.com>
2024-09-27 02:00:23 +0900

    <feat> : redis설정 추가
    로그인후 회원정보를 세션에 담기위해서 redis를 사용했다.
    redis를 쓰기위한 설정정보와 구성을 추가했다.

---
+++ Desktop/study/memoryMirror/src/main/java/com/toyproject/memoryMirror/web/config/RedisConfiguration.java
@@ -0,0 +1,26 @@
+package com.toyproject.memoryMirror.web.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class RedisConfiguration {
+
+    @Value("${spring.data.redis.host}")
+    private String host;
+
+    @Value("${spring.data.redis.port}")
+    private int port;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration(host,port);
+        return new LettuceConnectionFactory(redisStandaloneConfiguration);
+    }
+}
