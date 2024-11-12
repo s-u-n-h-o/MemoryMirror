1ebc8c2b01991b46dabce1e034aa4371f11dc726
s-u-n-h-o <chltjsgh06@gmail.com>
2024-11-13 00:13:37 +0900

    <chore> : 주석및 설정파일 수정
    1. 실행파일 주석 추가
    2. redis 공통모듈 추가

--- Desktop/study/memoryMirror/src/main/java/com/toyproject/memoryMirror/MemoryMirrorApplication.java
+++ Desktop/study/memoryMirror/src/main/java/com/toyproject/memoryMirror/MemoryMirrorApplication.java
@@ -4,12 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
-@EnableRedisHttpSession
+@EnableRedisHttpSession //redis에 세션데이터 저장하기 위해서 필요함
 @SpringBootApplication
 public class MemoryMirrorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MemoryMirrorApplication.class, args);
 	}
-
 }
--- Desktop/study/memoryMirror/src/main/java/com/toyproject/memoryMirror/web/config/RedisConfiguration.java
+++ Desktop/study/memoryMirror/src/main/java/com/toyproject/memoryMirror/web/config/RedisConfiguration.java
@@ -1,15 +1,19 @@
 package com.toyproject.memoryMirror.web.config;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.stereotype.Component;
 
 @Component
-@Slf4j
+@Configuration
 public class RedisConfiguration {
 
     @Value("${spring.data.redis.host}")
@@ -18,9 +22,33 @@ public class RedisConfiguration {
     @Value("${spring.data.redis.port}")
     private int port;
 
+    @Value("${spring.data.redis.password}")
+    private String password;
+
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration(host,port);
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName(host);
+        redisStandaloneConfiguration.setPort(port);
+        redisStandaloneConfiguration.setPassword(password);
         return new LettuceConnectionFactory(redisStandaloneConfiguration);
+    }
+
+    @Bean
+    public StringRedisTemplate stringRedisTemplate() {
+        StringRedisTemplate stringRedisTemplate = new StringRedisTemplate();
+
+        stringRedisTemplate.setConnectionFactory(redisConnectionFactory());
+
+        stringRedisTemplate.setKeySerializer(new StringRedisSerializer());
+        stringRedisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        stringRedisTemplate.setDefaultSerializer(new StringRedisSerializer());
+        stringRedisTemplate.afterPropertiesSet();
+        return stringRedisTemplate;
+    }
+
+    @Bean
+    public RedisSerializer<Object> springSessionDefaultRedisSerializer() {
+        return new GenericJackson2JsonRedisSerializer();
     }
 }
