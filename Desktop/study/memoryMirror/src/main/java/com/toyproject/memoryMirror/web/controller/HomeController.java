8a080d95750ab0f58614801aab956fbb2d830694
s-u-n-h-o <chltjsgh06@gmail.com>
2024-11-13 00:10:11 +0900

    <feat> : 로그인 후 redis session 추가
    1. 로그인후 저장된 앨범을 조회하기 위해서 session에 저장하는 로직 추가

--- Desktop/study/memoryMirror/src/main/java/com/toyproject/memoryMirror/web/controller/HomeController.java
+++ Desktop/study/memoryMirror/src/main/java/com/toyproject/memoryMirror/web/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.toyproject.memoryMirror.web.controller;
 
 import com.toyproject.memoryMirror.domain.model.user.User;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,5 +24,17 @@ public class HomeController {
     public String signupPage(Model model) {
         model.addAttribute("user", User.builder().build());
         return "signup";
+    }
+
+    @GetMapping("/home")
+    public String home() {
+        return "home";
+    }
+
+    @GetMapping("/album")
+    public String album(HttpSession httpSession, Model model) {
+        model.addAttribute("albumDetails", httpSession.getAttribute("albumDetails"));
+        model.addAttribute("albumInfo", httpSession.getAttribute("albumInfo"));
+        return "album";
     }
 }
